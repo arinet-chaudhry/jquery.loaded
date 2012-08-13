e4e71a44efaaeb5b305ecb853ecf469a7f44138c
Nadeem Chaudhry <chaudhry@arinet.com>
2012-08-13 11:55:29 -0700

    Added jQuery plugin

--- README.md
+++ README.md
@@ -2,3 +2,28 @@ jquery.loaded
 =============
 
 jQuery plugin for performing onload handling of dynamic content.
+
+Example usage:
+-------------
+
+```js
+(function ($) {
+	$(window).load(function() {
+		$('#container').load('loaded.html', function() {
+			$(window).load(function() {
+				alert('this will not fire');
+			});
+			$('#container').loaded(function() {
+				alert('this will fire');
+			});
+		});
+	});
+})(jQuery);
+```
+
+Also see the sample.html for additional examples.
+
+Manner behind the madness:
+-------------
+
+The jQuery event for window.load() only fires on the initial load and doesn't take into account AJAX calls. When implementing <a href="http://flexslider.woothemes.com/">FlexSlider</a> or other UI related functionality that requires images to be loaded, this plugin can be used instead.
---
+++ jquery.loaded.js
@@ -0,0 +1,27 @@
+﻿// Content loaded
+(function ($) {
+    $.fn.loaded = function (options) {
+        var
+            defaults = {
+                callback: function () { },
+                // based on supported tags http://www.w3schools.com/jsref/event_onload.asp
+                tags: ['frame','frameset','iframe','img','input[type="image"]','link','script','style']
+            },
+            settings = {};
+        if ($.isFunction(options))
+            settings = $.extend(defaults, { callback: options });
+        else
+            settings = $.extend(defaults, options);
+
+        var items = $(settings.tags.join(), this);
+        var itemsLength = items.length;
+        var itemsLoaded = 0;
+        items.each(function () {
+            $(this).load(function () {
+                if (++itemsLoaded == itemsLength) {
+                    settings.callback();
+                }
+            });
+        });
+    };
+})(jQuery);
