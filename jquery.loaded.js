fb2f91c5c70c4cafadcffbff5ae4c2af4488c194
Nadeem Chaudhry <chaudhry@arinet.com>
2012-09-06 06:55:11 -0700

    Improved reliability and bug fixes
    Ability to reference 'this' in the callback function.
    Fall back to window load event.
    Checking for complete items when supported.

--- jquery.loaded.js
+++ jquery.loaded.js
@@ -4,13 +4,13 @@
         var
             defaults = {
                 callback: function () { },
-				// tags to monitor
-                tags: ['frame','frameset','iframe','img','input[type="image"]','link','script','style'],
-				// events to monitor by tag
-				//	based on supported tags http://www.w3schools.com/jsref/event_onload.asp
-				load: ['frame','frameset','iframe','img','input[type="image"]','link','script','style'],
-				//	based on supported tags http://www.w3schools.com/jsref/event_onerror.asp
-				error: ['img','object','script','style']
+                    // tags to monitor
+                    tags: ['frame','frameset','iframe','img','input[type="image"]','link','script','style'],
+                    // events to monitor by tag
+                    //     based on supported tags http://www.w3schools.com/jsref/event_onload.asp
+                    load: ['frame','frameset','iframe','img','input[type="image"]','link','script','style'],
+                    //     based on supported tags http://www.w3schools.com/jsref/event_onerror.asp
+                    error: ['img','object','script','style']
             },
             settings = {};
         if ($.isFunction(options))
@@ -18,20 +18,41 @@
         else
             settings = $.extend(defaults, options);
 
-        var items = $(settings.tags.join(), this);
-        var itemsLength = items.length;
-        var itemsLoaded = 0;
-        var callback = function () {
-            if (++itemsLoaded == itemsLength) {
-                settings.callback();
-            }
-        };
-
-        items.each(function () {
-			$this = $(this);
-			var tagName = this.tagName.toLowerCase();
-            if ($.inArray(tagName, settings.load) >= 0) { $this.load(callback); }
-            if ($.inArray(tagName, settings.error) >= 0) { $this.error(callback); }
-        });
+          this.each(function() {
+               var self = this;
+               var items = $(settings.tags.join(), self);
+               var itemsLength = items.length;
+               var itemsLoaded = 0;
+               var complete = false;
+               var callback = function () {
+                    if (++itemsLoaded >= itemsLength && !complete) {
+                         settings.callback.call(self);
+                         complete = true;
+                    }
+               };
+     
+               items.each(function () {
+                    $this = $(this);
+                    var tagName = this.tagName.toLowerCase();
+                    if ($.inArray(tagName, settings.load) >= 0) {
+                         $this.one('load', callback);
+                         if (
+                              (typeof this.complete != "undefined" && this.complete) ||
+                              (jQuery.browser.msie && parseInt(jQuery.browser.version) == 6)
+                              ) {
+                              $this.trigger('load');
+                         }
+                    }
+                    if ($.inArray(tagName, settings.error) >= 0) {
+                         $this.one('error', callback);
+                    }
+               });
+               $(window).one('load', function() {
+                    if (!complete) {
+                         settings.callback.call(self);
+                         complete = true;
+                    }
+               });
+          });
     };
 })(jQuery);
